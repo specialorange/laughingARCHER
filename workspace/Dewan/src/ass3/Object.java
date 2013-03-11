ca13f2590aa22e85a6e40e7ccf78d967c03ef079
Chris Frisina <github@specialorange.org>
2013-03-10 20:59:26 -0400

    state and stateless objects.   Confusion around how to call other classes, a little about state, and what is bean conventions……

---
+++ workspace/Dewan/src/ass3/Object.java
@@ -0,0 +1,33 @@
+package ass3;
+
+public class Object {
+
+	//	are these scoped correctly?
+	private String attr1;
+	public String attr2;
+	
+	public static void main(String[] cheese) {
+		//cant return anything on a void method, so pass this stuff to another method
+		instantiator(cheese);
+	}
+
+	
+	//Create/instantiate the object 
+	private static Object instantiator(String[] cheese) {
+		//how do i declare/access the new object type? which scope?
+		var instantiatedObject = new Object(); //this is what i want to happen, but dont know how....
+		instantiatedObject.attr1 = new String();
+		instantiatedObject.attr2 = new String();
+		return instantiatedObject;
+	}
+
+	public void setAttr1(String stringToBeStored) {
+		this.attr1 = stringToBeStored;
+	}
+
+	public String getAttr2() {
+		return attr2;
+	}
+
+
+}
---
+++ workspace/Dewan/src/ass3/State.java
@@ -0,0 +1,43 @@
+package ass3;
+
+public class State {
+
+  public static void main(String[] cheese) {
+    if (cheese.length == 1) {
+        tokensInArray(cheese[0]);
+      } else {
+        System.out.println("Illegal no of arguments:" + cheese.length + ". Terminating program");
+      }
+  }
+
+  public static String tokensInArray (String cheese) {
+    String concatenation = new String();
+    boolean inAToken = false;
+    int startMarker = 0;
+    String tokenType = new String();
+    for ( int index = 0; index < cheese.length(); index++ ) {
+      char tokenChar = cheese.charAt(index);
+      if (Character.isDigit(tokenChar)) {
+	      if (!inAToken) { 
+	        startMarker = index;
+	        tokenType = "Number: ";
+	        inAToken = true;    
+	      }
+      } else if (Character.isLetter(tokenChar)) {
+	      if (!inAToken) { 
+	        startMarker = index;
+	        tokenType = "Word: ";
+	        inAToken = true;          
+	      }       
+      } else { //blank
+	      if (inAToken) { 
+	        concatenation += " " + tokenType + cheese.substring(startMarker, index);
+	        inAToken = false;
+	        tokenType = "";
+	      }
+      }
+    }
+    System.out.println(concatenation);
+    return concatenation;
+  }
+}
